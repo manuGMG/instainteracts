b3d9648e9eb7ba0ce94c9ad7c91a4864c808963b
Manuel <manurz2121@gmail.com>
2023-02-22 19:16:37 -0300

    Group all test_methods back in a single function

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -14,21 +14,15 @@ def insta():
     password = os.environ.get('I_PASSWORD')
     if username and password: return InstaInteracts(username, password)
 
-def test_comment_by_hashtag(insta):
+def test_methods(insta):
     assert insta
     insta.comment_by_hashtag('instagram', [u'🔥🔥'], limit=1)
     insta.comment_by_hashtag('instagram', [u'🔥'], limit=1, only_recent=True)
 
-def test_follow_by_hashtag(insta):
-    assert insta
     insta.follow_by_hashtag('instagram', limit=1)
     insta.follow_by_hashtag('instagram', limit=1, only_recent=True)
 
-def test_like_by_hashtag(insta):
-    assert insta
     insta.like_by_hashtag('instagram', limit=1)
     insta.like_by_hashtag('instagram', limit=1, only_recent=True)
 
-def test_unfollow(insta):
-    assert insta
     insta.unfollow(2)
